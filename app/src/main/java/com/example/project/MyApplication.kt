d0646d10377e0435fb310942ed2796331acf724e
Jongmyeung Lee <danieljml7love@gmail.com>
2024-01-23 23:36:57 +0900

    test

---
+++ app/src/main/java/com/example/project/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.project
+
+import android.app.Application
+import com.google.firebase.FirebaseApp
+
+class Application : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this) // firebase 초기화
+    }
+}
